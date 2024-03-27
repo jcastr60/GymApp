660606dab48a3e158a2f248133760d93b70653d9
jcastr60 <jasminecastro016@gmail.com>
2024-03-27 01:53:41 -0500

    Added file and folder structure

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 import './App.css';
+import ExcerciseDetail from './pages/ExcerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer';
+
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{width: {xl: '1488'}}} m="auto">
     <Navbar />
     <Routes>
       <Route path = "/" element = {<Home />} />
       {/* Dynamically allocate excersie id*/}
-      <Route path=" /excercise/:id" element={<ExcersideDetail />}/>
+      <Route path=" /excercise/:id" element={<ExcerciseDetail />}/>
     </Routes>
+    <Footer />
     </Box>
   )
 }
